17072097d67404147842d611dc3a73f6421fc3a4
Atsushi Morimoto <74th.tech@gmail.com>
2026-03-08 19:30:12 +0900

    SKILLの利用

---
+++ claude_agent_skills/main.py
@@ -0,0 +1,30 @@
+from claude_agent_sdk import (
+    ClaudeSDKClient,
+    ClaudeAgentOptions,
+)
+import asyncio
+
+prompt = """
+あなたは音声AIアシスタントのスタックチャンです。
+ユーザの質問に対して、3文程度の言葉で答えてください。
+音声案内であるため、マークダウンや絵文字等は用いずに、文字列だけで回答してください
+"""
+
+agent_option = ClaudeAgentOptions(
+    model="claude-haiku-4-5-20251001",
+    system_prompt=prompt,
+    cwd="./workspace",
+    setting_sources=["project"],
+    permission_mode="bypassPermissions",
+)
+
+
+async def main():
+    async with ClaudeSDKClient(options=agent_option) as client:
+        await client.query("明日の天気は？")
+        async for message in client.receive_response():
+            print(message)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
